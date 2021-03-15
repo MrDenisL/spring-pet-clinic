cad4335fa232d28088237d6598df4036c7488b89
denis <denis_lupashko@live.com>
2021-03-15 17:48:40 +0200

    adding base entity. closes #9

--- pet-clinic-data/src/main/java/denis/guru/spring/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/denis/guru/spring/petclinic/model/Person.java
@@ -3,7 +3,7 @@ package denis.guru.spring.petclinic.model;
 import lombok.Data;
 
 @Data
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
