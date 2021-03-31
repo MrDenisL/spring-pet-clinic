c019091642a0bba9c4cfef0ebdfe145f8c179c0e
denis <denis_lupashko@live.com>
2021-03-31 20:54:50 +0300

    update owner with jpa entity mappings. closes #35

--- pet-clinic-data/src/main/java/denis/guru/spring/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/denis/guru/spring/petclinic/model/Person.java
@@ -2,10 +2,16 @@ package denis.guru.spring.petclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 @Data
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name ="last_name")
     private String lastName;
 
 }
--- pet-clinic-data/src/main/java/denis/guru/spring/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/denis/guru/spring/petclinic/model/PetType.java
@@ -3,9 +3,15 @@ package denis.guru.spring.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 @Setter
 @Getter
 public class PetType extends BaseEntity {
-
+    @Column(name ="name")
     private String name;
 }
